dc80ec672e51ed2fc0d7c241aa0520cd84dbf15e
Marek <mrkkr91@gmail.com>
2019-04-13 15:33:03 +0200

    Define Faker Car data

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Car::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->lastName,
+        'price' => $faker->randomFloat(2, 40, 200),
     ];
 });
