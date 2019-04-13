476b92ba750ce8889f2f258660e72eb61647e123
Marek <mrkkr91@gmail.com>
2019-04-13 16:35:54 +0200

    Migration reservations table to DB

---
+++ database/migrations/2019_04_13_140206_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('car_id')->unsigned();
+            $table->dateTime('from_date');
+            $table->dateTime('to_date');
+            $table->timestamps();
+            $table->foreign('car_id')->references('id')->on('cars')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
