3810fee3ff04b7317ae986899ddd970a350441cd
Marek <mrkkr91@gmail.com>
2019-04-14 23:30:29 +0200

    Save date from Form to DB

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,23 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Http\Request;
+use App\Reservation;
 
 Route::get('/', function () {
     $cars = \App\Car::all();
 
     return view('home', ['cars' => $cars]);
+});
+
+Route::post('/reserve', function (Request $request) {
+    $car_id = $request->input('carsFormSelect');
+
+    $reservation = new Reservation;
+    $reservation->car_id = $car_id;
+    $reservation->from_date = $request->from_date;
+    $reservation->to_date = $request->to_date;
+    $reservation->save();
+
+    return redirect('/');
 });
